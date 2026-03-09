e89526ec30a1759075f74c5d34072ff7dc2593c9
motshekhene <juniorsebetola253@gmail.com>
2026-03-09 15:53:34 +0200

    get with id

--- 2822481-lab5.js
+++ 2822481-lab5.js
@@ -4,21 +4,44 @@ const PORT=3000;
 
 app.use(express.json());
 
+
+
+let books = [];
 // Your routes here
 
 app.get('/whoami',(req,res)=>{
 
     const student={
-        "studentNumber":"2822481"
+        studentNumber:"2822481"
     }
 
-    res.send(student);
+    //res.send(student);
+    res.status(200).send(student);
+});
+
+app.get('/books',(req,res)=>{
+    res.send(books);
+});
+
+
+app.get('books/:id',(res,req)=>{
+    const idBook=req.params.id;
+    const book=books.find(b=>b.id===idBook);
+
+    if(!book){
+        return res.status(404).json({error:"Book not found"});
+    }
+    res.status(200).json(book);
+    
 })
+
+
+
+
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
 
 
-let books = [];
 
